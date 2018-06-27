25a1a1ff9120adfbb12a8ab3c1519c26c5b51f37
Will Kahn-Greene <willkg@mozilla.com>
2018-06-27 15:36:50 -0400

    Fix missing parse_version import

--- siggen/__init__.py
+++ siggen/__init__.py
@@ -2,6 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from pkg_resources import parse_version
+
+
 # yyyymmdd
 __releasedate__ = '20180627'
 
