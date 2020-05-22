25274ac19fc0612e03510f4186b02b4edc876418
Will Kahn-Greene <willkg@mozilla.com>
2020-05-22 13:53:10 -0400

    Update for 1.0.3 release

--- siggen/__init__.py
+++ siggen/__init__.py
@@ -6,8 +6,8 @@ from pkg_resources import parse_version
 
 
 # yyyymmdd
-__releasedate__ = '20200207'
+__releasedate__ = '20200522'
 
 # x.y.z or x.y.z.dev0 -- semver
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 VERSION = parse_version(__version__)
